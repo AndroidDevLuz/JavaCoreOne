830d6f0260d8fc191e5108179263e666d0c7381a
NLUZYA15 <nikolay.luzyanin@decathlon.com>
2020-04-20 19:32:44 +0300

    lesson5

---
+++ src/lesson5/Employees.java
@@ -0,0 +1,26 @@
+package lesson5;
+
+public class Employees {
+
+    private static Person[] persArray;
+
+    public static void main(String[] args) {
+        fillAdapter();
+        printPersonFields(persArray);
+    }
+
+    private static void fillAdapter() {
+        persArray = new Person[5];
+        persArray[0] = new Person("Иванов Иван","Инженер", "ii@gmail.com", "+79996669966",30000, 67);
+        persArray[1] = new Person("Петров Вася","Инженер", "pv@gmail.com", "+79996669966",30000, 21);
+        persArray[2] = new Person("Благодарный Леха","Инженер", "sl@gmail.com", "+79996669966",30000, 45);
+        persArray[3] = new Person("Лузянин Николай","Developer", "nl9166420458@gmail.com", "+79166420458",40000, 36);
+        persArray[4] = new Person("Бывалый Игорь","Инженер", "ii@gmail.com", "+79996669966",30000, 44);
+    }
+
+    private static void printPersonFields(Person[] array) {
+        for(int i=0; i < 5; i++){
+            array[i].showInfo();
+        }
+    }
+}
